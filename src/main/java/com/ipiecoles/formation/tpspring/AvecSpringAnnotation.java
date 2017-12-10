7b3881ffe896e774c4d0400e1fef01d0f8817372
Alexandre <alexandre.petrillo@gmail.com>
2017-12-10 19:37:19 +0100

    Configuration avec spring annotation avec Scope

--- src/main/java/com/ipiecoles/formation/tpspring/AvecSpringAnnotation.java
+++ src/main/java/com/ipiecoles/formation/tpspring/AvecSpringAnnotation.java
@@ -1,35 +1,24 @@
 package com.ipiecoles.formation.tpspring;
 
-import com.ipiecoles.formation.tpspring.model.Film;
 import com.ipiecoles.formation.tpspring.service.RecommandationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AvecSpringAnnotation {
 
     @Autowired
-    @Qualifier("RecommandationServiceTriche")
-    private RecommandationService recommandationService;
+    private RecommandationService recommandationService1;
+    @Autowired
+    private RecommandationService recommandationService2;
 
     public static void main(String... args) {
-        {
-            AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.ipiecoles.formation.tpspring");
-            RecommandationService recommandationService = applicationContext.getBean("RecommandationServiceTriche", RecommandationService.class);
-            Film recommandation = recommandationService.getRecommandation(1L);
-            System.out.println(recommandation);
-            applicationContext.close();
-        }
-        {
-            AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.ipiecoles.formation.tpspring");
-            AvecSpringAnnotation avecSpringAnnotation = applicationContext.getBean(AvecSpringAnnotation.class);
-            Film recommandation = avecSpringAnnotation.recommandationService.getRecommandation(1L);
-            System.out.println(recommandation);
-            applicationContext.close();
-        }
-
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.ipiecoles.formation.tpspring");
+        AvecSpringAnnotation bean = applicationContext.getBean(AvecSpringAnnotation.class);
+        System.out.println(bean.recommandationService1.getRecommandation(1L));
+        System.out.println(bean.recommandationService2.getRecommandation(1L));
+        applicationContext.close();
     }
+
 }
--- src/main/java/com/ipiecoles/formation/tpspring/service/RecommandationService.java
+++ src/main/java/com/ipiecoles/formation/tpspring/service/RecommandationService.java
@@ -1,9 +1,44 @@
 package com.ipiecoles.formation.tpspring.service;
 
 import com.ipiecoles.formation.tpspring.model.Film;
+import com.ipiecoles.formation.tpspring.model.User;
+import com.ipiecoles.formation.tpspring.repository.UserRepository;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
-public interface RecommandationService {
+import javax.annotation.PostConstruct;
+import java.util.List;
 
-    Film getRecommandation(Long idUser);
+@Service("RecommandationService")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class RecommandationService {
+
+	private FilmService filmService;
+	private UserRepository userRepository;
+
+	public RecommandationService(FilmService filmService, UserRepository userRepository) {
+		this.filmService = filmService;
+		this.userRepository = userRepository;
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("RecommandationServiceImpl PostConstruct");
+	}
+
+	public Film getRecommandation(Long idUser) {
+		User user = userRepository.findById(idUser);
+		for (Film film : user.getFilmsAVoir()) {
+			return film;
+		}
+		List<Film> nouveautes = filmService.findNouveautesParType(user.getType());
+		for (Film film : nouveautes ) {
+			if(!user.getDejaVus().contains(film)) {
+				return film;
+			}
+		}
+		return null;
+	}
 
 }
